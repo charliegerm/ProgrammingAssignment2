9b7b10139a2b5216173a28d190e869d9b19b1056
Charlie Germano <cgermano@gmail.com>
2015-01-25 15:26:10 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Accept and input matrix, determine the inverse, and create a cache containing that inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(inputMatrix = matrix()) {
+  cacheMatrix <- NULL
+  
+  set <- function(mySet) {
+    inputMatrix <<- externalMatrix
+    cacheMatrix <<- NULL
+  }
+  
+  get <- function() cacheMatrix
+  setInverse <- function(inverse) cacheMatrix <<- externalMatrix
+  getInverse <- function() cacheMatrix
+  
+  list(set= set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
+  
+  
 }
 
+## Accept an input matrix, determine if its inverse is contained in a local cache. If so, return the cache copy. 
+## If cache copy is dirty, refresh the cache and return the newly created inverse matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(inputMatrix, ...) {
+  cacheMatrix <- inputMatrix$getInverse
+  if (!is.null(cacheMatrix)) {
+    return(cacheMatrix)
+    
+  }
+  
+  myMatrix <- inputMatrix$get()
+  cacheMatrix <- solve(myMatrix, ...)
+  inputMatrix <- setInverse(cacheMatrix)
+  
+  cacheMatrix
+  
 }
